cd0e4548709109eae839ba43dcaae3da775d3c33
f1eeman <mr.f1eeman@gmail.com>
2021-02-15 18:39:02 +0600

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,18 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 const config = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+    contentBase: path.resolve(__dirname, 'dist'),
+  }
 };
 
 export default config;
